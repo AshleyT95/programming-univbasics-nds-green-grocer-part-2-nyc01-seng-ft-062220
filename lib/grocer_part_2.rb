740d7e2721381b0942e16269a769e64a5d0a1717
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-06-17 23:57:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -63,9 +63,9 @@ def checkout(cart, coupons)
   while counter < final_cart.length
     total += final_cart[counter][:price] = final_cart[counter][:count]
     counter += 1
-  endif total > 100
+  end
+  if total > 100
   total -= (total * 0.10)
 end
 total
-
 end
