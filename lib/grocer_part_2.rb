7939232656835850559ed9ae67b6038b2ad23ee6
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-06-18 00:15:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,8 +7,8 @@ def apply_coupons(cart, coupons)
   counter=0
   while counter < coupons.length
     cart_item= find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name= "#{coupons[counter][:item]} W/COUPON"
-    cart_item_with_coupon= find_item_by_name_in_collection(couponed_item_name, cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
       if cart_item_with_coupon
         cart_item_with_coupon[:count] += coupons[counter][:num]
@@ -22,7 +22,8 @@ def apply_coupons(cart, coupons)
         }
         cart << cart_item_with_coupon
         cart_item [:count] -= coupons[counter][:num]
-      endend
+      end
+    end
       counter += 1
     end
     cart
@@ -68,3 +69,4 @@ def checkout(cart, coupons)
   total -= (total * 0.10)
 end
 total
+end
